e84fd80fcc125c624e382eac516f9c8f52478311
TigerDevCom <antonio.lee327@gmail.com>
2016-11-08 07:29:27 -0800

    revoke change

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "welcome#index"
+  #get 'book/index'
+
+  #definition of resources for book management
+  resources :books
 end
